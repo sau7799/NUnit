a775b47253b90010d4b48a59e86469947bb9196a
HP <HP@DESKTOP-CSV2EHJ>
2022-05-24 14:58:55 +0530

    Add project files.

---
+++ Nunit.Demo.Text/CalculatorTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using NunitDemo;
+namespace Nunit.Demo.Test
+
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test, Order(2)]
+        public void Test_Addition_With_Valid_Intigers()
+        {
+            var calc = new Calculator();
+            var result = calc.Addition(3, 5);
+            Assert.AreEqual(8, result);
+
+        }
+        [Test,Order(1)]
+        public void Test_Substraction_Argument_Exception()
+        {
+            var calc = new Calculator();
+            Assert.Catch<SystemException>(() => calc.Subtraction(4, 5));
+            Assert.Throws<ArgumentException>(() => calc.Subtraction(4, 5));
+        }
+
+        [TestCase(1, 2, 3)]
+        [TestCase(2, 3, 5)]
+        [TestCase(3, 4, 7)]
+        public void Test_Addition_Multiple(int first, int second, int expectedresult)
+        {
+            var calc = new Calculator();
+            var calculated = calc.Addition(first, second);
+            Assert.AreEqual(expectedresult, calculated);
+        }
+
+        [Ignore("Ignore test")]
+        public void Test_To_Ignore()
+        {
+        }
+
+    }
+}
---
+++ NunitDemo/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace NunitDemo
+{
+    public class Calculator
+    {
+        public int Addition(int first, int second)
+        {
+            return first + second;
+        }
+
+        public int Subtraction(int first, int second)
+        {
+            if (first < second)
+                throw new ArgumentException($"First number {first} is less than second number {second}");
+
+            return first - second;
+        }
+    }
+
+}
